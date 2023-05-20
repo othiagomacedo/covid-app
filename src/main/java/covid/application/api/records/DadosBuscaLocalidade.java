c60a05081a16ace4fd091a41167c5877d5906da4
Thiago Chirana Macedo <96755607+othiagomacedo@users.noreply.github.com>
2023-05-20 12:50:40 -0300

    [IMPROVEMENT] Implementar Anotacoes de verificacao

--- src/main/java/covid/application/api/records/DadosBuscaLocalidade.java
+++ src/main/java/covid/application/api/records/DadosBuscaLocalidade.java
@@ -1,10 +1,14 @@
 package covid.application.api.records;
 
+import covid.application.api.anotations.Data;
+
 public record DadosBuscaLocalidade(
         String nomeLocalidade,
 
+        @Data
         String dataInicial,
 
+        @Data
         String dataFinal
 ) {
 }
