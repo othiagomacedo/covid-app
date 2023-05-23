73c88acaf9fbc125829efdbedf891890e460047b
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-22 23:55:45 -0300

    [IMPROVEMENT] Melhoria em Entidade do Banco de Dados

--- src/main/java/covid/application/api/modelos/entidade/HistoricoPais.java
+++ src/main/java/covid/application/api/modelos/entidade/HistoricoPais.java
@@ -1,6 +1,8 @@
 package covid.application.api.modelos.entidade;
 
 import covid.application.api.anotations.Data;
+import covid.application.api.modelos.records.DadosBuscaPaisDatas;
+import covid.application.api.modelos.records.DadosRespostaReportPais;
 import jakarta.persistence.*;
 
 @Entity(name = "historico_pais")
@@ -26,14 +28,34 @@ public class HistoricoPais {
     @Column(name = "ultimo_update")
     private String ultimoUpdate;
 
-    @Column(name = "percent_fatalidade")
-    private float percentualFatalidade;
+    @Column(name = "percentual_fatalidade")
+    private double percentualFatalidade;
 
     @ManyToOne
     @JoinColumn(name = "pais_id")
     Pais paisId;
 
     public HistoricoPais() {
+    }
+
+    public HistoricoPais(DadosRespostaReportPais dados, Pais idPais){
+        this.confirmados = dados.confirmados();
+        this.mortes = dados.mortes();
+        this.recuperados = dados.recuperados();
+        this.ultimoUpdate = dados.ultUpdate();
+        this.percentualFatalidade = dados.taxaFatalidade();
+        this.paisId = idPais;
+    }
+
+    public HistoricoPais(DadosBuscaPaisDatas dataBuscas, DadosRespostaReportPais dados, Pais idPais){
+        this.dataInicial = dataBuscas.dataInicial();
+        this.dataFinal = dataBuscas.dataFinal();
+        this.confirmados = dados.confirmados();
+        this.mortes = dados.mortes();
+        this.recuperados = dados.recuperados();
+        this.ultimoUpdate = dados.ultUpdate();
+        this.percentualFatalidade = dados.taxaFatalidade();
+        this.paisId = idPais;
     }
 
 
