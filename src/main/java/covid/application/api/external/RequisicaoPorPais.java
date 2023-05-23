b49754aee2de8277768395cd078bbc4a6e576684
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-22 23:56:51 -0300

    [IMPROVEMENT] Melhoria em Requisicao na API

--- src/main/java/covid/application/api/external/RequisicaoPorPais.java
+++ src/main/java/covid/application/api/external/RequisicaoPorPais.java
@@ -2,47 +2,52 @@ package covid.application.api.external;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import covid.application.api.modelos.enums.Requisicao;
-import covid.application.api.modelos.records.DadosReportBuscaDataSigla;
+import covid.application.api.modelos.records.DadosPaisesSigla;
 import covid.application.api.modelos.records.DadosRespostaReportPais;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequisicaoPorPais{
 
-    public static DadosRespostaReportPais obterDadosPorDataSigla(DadosReportBuscaDataSigla dadosRecord) throws Exception {
-        String url = null;
-        DadosRespostaReportPais resposta = null;
-
-        url = Requisicao.REPORT_TOTAL_DIA_PAIS_POR_DATA_SIGLA.get();
-        DadosReportBuscaDataSigla dados = DadosReportBuscaDataSigla.obterCampos(dadosRecord);
-        url = url.replace("{DATA}",dados.data()).replace("{SIGLA}",dados.sigla());
-        String jsonResposta = Requests.realizarRequest(url);
-        resposta = montarRecord(jsonResposta);
-
-        return resposta;
-    }
-
-    private static DadosRespostaReportPais montarRecord(String respostaRequest) throws Exception {
+    public static DadosRespostaReportPais montarRecordRespostaDadosPais(String respostaRequest) throws Exception {
         try{
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode jsonNode = objectMapper.readTree(respostaRequest);
             JsonNode dataNode = jsonNode.get("data");
             DadosRespostaReportPais dados = new DadosRespostaReportPais(
+                    "",
                     dataNode.get("date").asText(),
                     dataNode.get("last_update").asText(),
                     dataNode.get("confirmed").asInt(),
-                    dataNode.get("confirmed_diff").asInt(),
                     dataNode.get("deaths").asInt(),
-                    dataNode.get("deaths_diff").asInt(),
                     dataNode.get("recovered").asInt(),
-                    dataNode.get("recovered_diff").asInt(),
-                    dataNode.get("active").asInt(),
-                    dataNode.get("active_diff").asInt(),
                     dataNode.get("fatality_rate").asDouble()
             );
             return dados;
         }catch (Exception e){
             throw new Exception("Nao foi possível obter Record DadosRespostaReportPais a partir de String",e);
         }
+    }
+
+    public static List<DadosPaisesSigla> montarRecordPaisesSigla(String respostaRequest)throws Exception{
+        try{
+            List<DadosPaisesSigla> lista = new ArrayList<>();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(respostaRequest);
+            JsonNode dataNode = jsonNode.get("data");
+            for (JsonNode no : dataNode){
+                DadosPaisesSigla dados = new DadosPaisesSigla(
+                        no.get("name").asText(),
+                        no.get("iso").asText()
+                );
+                lista.add(dados);
+            }
+            return lista;
+        }catch (Exception e){
+            throw new Exception("Nao foi possível obter Record DadosPaisesSigla a partir de String",e);
+        }
+
     }
 
 
