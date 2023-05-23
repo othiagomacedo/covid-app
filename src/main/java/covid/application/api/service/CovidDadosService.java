c5318974f5cd7710b17fd87b45c68c20ca19e4e2
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-22 23:52:59 -0300

    [IMPROVEMENT] Melhoria dos Services ao projeto

--- src/main/java/covid/application/api/service/CovidDadosService.java
+++ src/main/java/covid/application/api/service/CovidDadosService.java
@@ -1,15 +1,135 @@
 package covid.application.api.service;
 
-import covid.application.api.modelos.records.DadosBuscaLocalidade;
+import covid.application.api.external.Requests;
+import covid.application.api.external.RequisicaoPorPais;
+import covid.application.api.modelos.entidade.HistoricoPais;
+import covid.application.api.modelos.entidade.Pais;
+import covid.application.api.modelos.enums.Requisicao;
+import covid.application.api.modelos.records.DadosBuscaPaisDatas;
+import covid.application.api.modelos.records.DadosPaisesSigla;
+import covid.application.api.modelos.records.DadosReportBuscaDataSigla;
+import covid.application.api.modelos.records.DadosRespostaReportPais;
+import covid.application.api.repository.HistoricoBenchmarkRepository;
+import covid.application.api.repository.HistoricoPaisRepository;
+import covid.application.api.repository.PaisRepository;
+import covid.application.api.threads.RequestThread;
+import covid.application.api.threads.RequestThreadRepository;
+import covid.application.api.util.Datas;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class CovidDadosService {
 
-    public static ResponseEntity obterDadosByData(DadosBuscaLocalidade dados){
-        //
+    @Autowired
+    private HistoricoPaisRepository histPais;
 
-        return ResponseEntity.ok().build();
+    @Autowired
+    private HistoricoBenchmarkRepository bench;
+
+    @Autowired
+    private PaisRepository pais;
+
+    private static Logger LOG = LoggerFactory.getLogger(CovidDadosService.class);
+
+
+    public ResponseEntity obterDadosByData(DadosReportBuscaDataSigla dados) throws Exception {
+
+        //Verifica se sigla existe no banco, se não então é certo que não tenha nenhum historico de pesquisa já salvo
+        String sigla = dados.sigla().toUpperCase();
+
+        LOG.info("Verificacao se país com  sigla "+sigla+ " existe localmente");
+        var paisE = pais.findBySigla(sigla);
+
+        if (paisE.isEmpty()){
+            LOG.info("Não encontrei o pais pela Sigla "+dados.sigla().toUpperCase()+", vou iniciar uma busca na API externa");
+            String retorno = Requests.realizarRequest(Requisicao.OBTER_TODOS_PAISES_E_SIGLAS.get());
+            List<DadosPaisesSigla> listaDados = RequisicaoPorPais.montarRecordPaisesSigla(retorno);
+
+            for (DadosPaisesSigla dadosP : listaDados){
+                if (dadosP.sigla().equalsIgnoreCase(sigla) || dadosP.sigla().contains(sigla)){
+                    LOG.info("Pais encontrado pela Sigla "+sigla.toUpperCase()+" na API Externa! "+dados+". O mesmo será persistido no banco local");
+                    pais.save(new Pais(dadosP));
+                }
+            }
+        }
+
+        return ResponseEntity.ok(dados);
+    }
+
+    public ResponseEntity obterDadosPaisPorFaixaDatas(DadosBuscaPaisDatas dadosBusca) throws Exception{
+        if (Datas.isSequencial(dadosBusca.dataInicial(),dadosBusca.dataFinal())){
+            LOG.info("Inicio para obter informacoes de País de sigla "+dadosBusca.sigla()+" por datas "+dadosBusca.dataInicial()+" até "+dadosBusca.dataFinal());
+            List<RequestThread> listaRun = new ArrayList<>();
+            List<DadosRespostaReportPais> listaDados = new ArrayList<>();
+            Date date = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+            long confirmados = 0;
+            long mortes = 0;
+            long recuperados = 0;
+
+
+            String url = Requisicao.REPORT_TOTAL_DIA_PAIS_POR_DATA_SIGLA.get();
+
+            String url1 = url.replace("{DATA}", dadosBusca.dataInicial()).replace("{SIGLA}", dadosBusca.sigla());
+            String url2 = url.replace("{DATA}", dadosBusca.dataFinal()).replace("{SIGLA}", dadosBusca.sigla());
+
+            listaRun.add(new RequestThread(url1));
+            listaRun.add(new RequestThread(url2));
+
+            LOG.info("Preparo para disparo de requisicoes a API externa para obter a informação completa. "+listaRun.size());
+
+            LOG.info("Iniciando requisicoes a API externa...");
+            for (RequestThread rt : listaRun){
+                rt.run();
+            }
+
+            List<String> results = RequestThreadRepository.getLista();
+            LOG.info("A requisicao obteve "+results.size()+" resultados. Vamos compilar em apenas 1 para o cliente");
+
+            DadosRespostaReportPais dadosPais1 = RequisicaoPorPais.montarRecordRespostaDadosPais(results.get(0));
+            DadosRespostaReportPais dadosPais2 = RequisicaoPorPais.montarRecordRespostaDadosPais(results.get(1));
+
+            //novo calculo da diferenca entre a primeira data com a segunda data
+            confirmados = dadosPais2.confirmados() - dadosPais1.confirmados();
+            mortes = dadosPais2.mortes() - dadosPais1.mortes();
+            recuperados = dadosPais2.recuperados() - dadosPais1.recuperados();
+
+            //Montar os dados no record DadosRespostaReportPais para retorno ao Cliente
+            DadosRespostaReportPais dadosTotal = new DadosRespostaReportPais(
+                    dadosBusca.sigla(),
+                    sdf.format(date),
+                    sdf.format(date),
+                    confirmados,
+                    mortes,
+                    recuperados,
+                    (mortes/(double)confirmados) * 100
+            );
+
+            LOG.info("Dados a serem enviados ao Cliente: "+dadosTotal);
+            LOG.info("Iremos persistir esse dados no Historico de Pais");
+            saveHistPais(dadosBusca,dadosTotal);
+
+            return ResponseEntity.ok(dadosTotal);
+
+        } else {
+            LOG.error("A data "+dadosBusca.dataInicial()+ " deve ser anterior a data "+dadosBusca.dataFinal());
+            return ResponseEntity.badRequest().body("A data "+dadosBusca.dataInicial()+ " deve ser anterior a data "+dadosBusca.dataFinal());
+        }
+    }
+
+    private void saveHistPais(DadosBuscaPaisDatas dataBuscas, DadosRespostaReportPais dadosTotal) throws Exception {
+        Pais paisAux = pais.findBySigla(dadosTotal.sigla()).get();
+        histPais.save(new HistoricoPais(dataBuscas,dadosTotal, paisAux));
+        LOG.info("Salvo histórico do pais "+paisAux.getNome());
     }
 }
--- src/main/java/covid/application/api/service/PaisService.java
+++ src/main/java/covid/application/api/service/PaisService.java
@@ -5,8 +5,6 @@ import covid.application.api.external.RequisicaoPorPais;
 import covid.application.api.modelos.entidade.Pais;
 import covid.application.api.modelos.enums.Requisicao;
 import covid.application.api.modelos.records.DadosPaisesSigla;
-import covid.application.api.modelos.records.DadosReportBuscaDataSigla;
-import covid.application.api.modelos.records.ListaDadosPaisesSigla;
 import covid.application.api.repository.PaisRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,33 +12,93 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
+import java.util.List;
+
 @Service
 public class PaisService {
 
     @Autowired
     private PaisRepository pais;
 
+    public PaisService() {
+    }
+
     private static Logger LOG = LoggerFactory.getLogger(PaisService.class);
 
-
     public ResponseEntity obterPaisByNome(String nome) throws Exception {
-        var paisE = pais.findByNome(nome);
+        var paisE = pais.findByNome(nome.toUpperCase());
+        DadosPaisesSigla dados;
 
         if (paisE.isEmpty()){
-            LOG.debug("Não encontrei o pais pelo nome "+nome.toUpperCase()+", vou iniciar uma busca na API externa");
+            boolean achou = false;
+
+            LOG.info("Não encontrei o pais pelo nome "+nome.toUpperCase()+", vou iniciar uma busca na API externa");
             String retorno = Requests.realizarRequest(Requisicao.OBTER_TODOS_PAISES_E_SIGLAS.get());
+            List<DadosPaisesSigla> listaDados = RequisicaoPorPais.montarRecordPaisesSigla(retorno);
+
+            for (DadosPaisesSigla dadosP : listaDados){
+                if (dadosP.nome().equalsIgnoreCase(nome) || dadosP.nome().contains(nome)){
+                    achou = true;
+                    dados = dadosP;
+                    LOG.info("Pais encontrado na API Externa! "+dados+". O mesmo será persistido no banco local e enviado ao Cliente");
+                    pais.save(new Pais(dadosP));
+                    return ResponseEntity.ok(dados);
+                }
+            }
+
+            if (!achou){
+                return ResponseEntity.badRequest().body("Pais com nome "+nome.toUpperCase()+" não encontrado");
+            }
+
+        } else {
+            Pais p = paisE.get();
+            dados = new DadosPaisesSigla(p.getNome(), p.getSigla());
+            LOG.info("Pais encontrado: "+dados+". Será enviado ao Cliente.");
+            return ResponseEntity.ok(dados);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity obterPaisBySigla(String sigla) throws Exception{
+        try{
+            return ResponseEntity.ok(obterPaisBySiglas(sigla));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Pais com sigla "+sigla + " não encontrado.");
+        }
+    }
+
+    public DadosPaisesSigla obterPaisBySiglas(String sigla)throws Exception{
+        var paisE = pais.findBySigla(sigla.toUpperCase());
+        DadosPaisesSigla dados;
+
+        if (paisE.isEmpty()){
+            LOG.info("Não encontrei o pais pela Sigla "+sigla.toUpperCase()+", vou iniciar uma busca na API externa");
+            String retorno = Requests.realizarRequest(Requisicao.OBTER_TODOS_PAISES_E_SIGLAS.get());
+            List<DadosPaisesSigla> listaDados = RequisicaoPorPais.montarRecordPaisesSigla(retorno);
+
+            for (DadosPaisesSigla dadosP : listaDados){
+                if (dadosP.sigla().equalsIgnoreCase(sigla) || dadosP.sigla().contains(sigla)){
+                    dados = dadosP;
+                    LOG.info("Pais encontrado pela Sigla "+sigla.toUpperCase()+" na API Externa! "+dados+". O mesmo será persistido no banco local e enviado ao Cliente");
+                    pais.save(new Pais(dados));
+                    return dadosP;
+                }
+            }
 
         }
 
         Pais p = paisE.get();
-        DadosPaisesSigla dados = new DadosPaisesSigla(p.getNome(), p.getSigla());
-        return ResponseEntity.ok(dados);
+        dados = new DadosPaisesSigla(p.getNome(), p.getSigla());
+        LOG.info("Pais encontrado pela sigla: "+dados+". Será enviado ao Cliente.");
+        return dados;
     }
 
-    public ResponseEntity obterPaisBySigla(String sigla){
-        var paisE = pais.findBySigla(sigla);
-        Pais p = paisE.get();
-        DadosPaisesSigla dados = new DadosPaisesSigla(p.getNome(), p.getSigla());
-        return ResponseEntity.ok(dados);
+    public ResponseEntity getTodosPaisesCadastrados() throws Exception {
+        LOG.info("Obtendo a lista de paises (nome e sigla) persistidos atualmente");
+        List<Pais> lista = pais.findAll();
+        LOG.info("Quantidade da Lista de paises: "+lista.size()+". Será enviada lista ao Cliente.");
+        return ResponseEntity.ok(lista);
     }
 }
