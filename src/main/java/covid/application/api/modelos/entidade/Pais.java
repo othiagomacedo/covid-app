118a9151a603c00680b9de852af0b8c1090d01d4
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-22 23:56:30 -0300

    [IMPROVEMENT] Melhoria em entidade do banco de Dados

--- src/main/java/covid/application/api/modelos/entidade/Pais.java
+++ src/main/java/covid/application/api/modelos/entidade/Pais.java
@@ -1,14 +1,17 @@
 package covid.application.api.modelos.entidade;
 
+import covid.application.api.modelos.records.DadosPaisesSigla;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "pais", uniqueConstraints = @UniqueConstraint(columnNames = "sigla"))
 public class Pais {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique = true)
     private String sigla;
 
     private String nome;
@@ -17,6 +20,11 @@ public class Pais {
     public Pais(String sigla, String nome) {
         this.sigla = sigla;
         this.nome = nome;
+    }
+
+    public Pais(DadosPaisesSigla dados){
+        this.sigla = dados.sigla().toUpperCase();
+        this.nome = dados.nome().toUpperCase();
     }
 
     public Pais() {
