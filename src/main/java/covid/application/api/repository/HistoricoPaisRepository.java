fa2a37a942e10e9f036587b922c94a208480082d
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-24 00:24:51 -0300

    [IMPROVEMENT] Melhoria de Repository

--- src/main/java/covid/application/api/repository/HistoricoPaisRepository.java
+++ src/main/java/covid/application/api/repository/HistoricoPaisRepository.java
@@ -6,7 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HistoricoPaisRepository extends JpaRepository<HistoricoPais, Long> {
 
+    @Query("SELECT h FROM historico_pais h WHERE 1=1 AND h.paisId = :pais AND h.dataInicial = :dataInicial AND h.dataFinal = :dataFinal")
+    Optional<HistoricoPais> findHistoricoPais(Pais pais, String dataInicial, String dataFinal);
+
+    @Query("SELECT h FROM historico_pais h WHERE 1=1 AND h.paisId = :pais AND h.dataInicial = :data")
+    Optional<HistoricoPais> findHistoricoPaisByDataUnica(Pais pais, String data);
 }
