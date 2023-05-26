e60dda04fbf94fb9886e60995cbfdb1c2b5bdee2
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-26 01:34:10 -0300

    [IMPROVEMENT] Melhoria para record ter melhor entrega

--- src/main/java/covid/application/api/modelos/records/DadosRespostaBenchmark.java
+++ src/main/java/covid/application/api/modelos/records/DadosRespostaBenchmark.java
@@ -1,6 +1,7 @@
 package covid.application.api.modelos.records;
 
 public record DadosRespostaBenchmark(
+        long id,
         String nomeBench,
         String dataHoraBenchmark,
         String dataInicial,
@@ -9,6 +10,26 @@ public record DadosRespostaBenchmark(
         long mortesDiferenca,
         long recuperadosDiferenca,
         DadosRespostaReportPais dadosPais1,
-        DadosRespostaReportPais dadosPais2
+        DadosRespostaReportPais dadosPais2,
+
+        boolean foiSalvo,
+
+        String mensagem
 ) {
+    public DadosRespostaBenchmark(long id, String nomeBench, String dataHoraBenchmark, String dataInicial, String dataFinal, long confirmadosDiferenca, long mortesDiferenca, long recuperadosDiferenca, DadosRespostaReportPais dadosPais1, DadosRespostaReportPais dadosPais2, boolean foiSalvo, String mensagem) {
+        this.id = id;
+        this.nomeBench = nomeBench;
+        this.dataHoraBenchmark = dataHoraBenchmark;
+        this.dataInicial = dataInicial;
+        this.dataFinal = dataFinal;
+        this.confirmadosDiferenca = confirmadosDiferenca;
+        this.mortesDiferenca = mortesDiferenca;
+        this.recuperadosDiferenca = recuperadosDiferenca;
+        this.dadosPais1 = dadosPais1;
+        this.dadosPais2 = dadosPais2;
+        this.foiSalvo = foiSalvo;
+        this.mensagem = mensagem;
+    }
+
+
 }
