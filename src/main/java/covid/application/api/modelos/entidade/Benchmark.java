c5f5f5ffe4fe92a2812c189e7ec704d46f34fb0f
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-26 01:31:38 -0300

    [IMPROVEMENT] Melhoria entidade Benchmark.java

--- src/main/java/covid/application/api/modelos/entidade/Benchmark.java
+++ src/main/java/covid/application/api/modelos/entidade/Benchmark.java
@@ -31,6 +31,15 @@ public class Benchmark {
     @Column(name = "data_final")
     String dataFinal;
 
+    @Column(name = "confirmados_diferenca")
+    long confirmadosDiferenca;
+
+    @Column(name = "mortes_diferenca")
+    long mortesDiferenca;
+
+    @Column(name = "recuperados_diferenca")
+    long recuperadosDiferenca;
+
     public Benchmark() {}
 
     public Benchmark(String nomeHistorico, HistoricoPais historicoPais1, HistoricoPais historicoPais2, String dataHistorico, String dataIncial, String dataFinal) {
@@ -49,5 +58,97 @@ public class Benchmark {
         this.dataHistorico = bench.dataHoraBenchmark();
         this.dataIncial = bench.dataInicial();
         this.dataFinal = bench.dataFinal();
+    }
+
+    public Benchmark(String nomeHistorico, HistoricoPais historicoPais1, HistoricoPais historicoPais2, String dataHistorico, String dataIncial, String dataFinal, long confirmadosDiferenca, long mortesDiferenca, long recuperadosDiferenca) {
+        this.nomeHistorico = nomeHistorico;
+        this.historicoPais1 = historicoPais1;
+        this.historicoPais2 = historicoPais2;
+        this.dataHistorico = dataHistorico;
+        this.dataIncial = dataIncial;
+        this.dataFinal = dataFinal;
+        this.confirmadosDiferenca = confirmadosDiferenca;
+        this.mortesDiferenca = mortesDiferenca;
+        this.recuperadosDiferenca = recuperadosDiferenca;
+    }
+
+    public String getNomeHistorico() {
+        return nomeHistorico;
+    }
+
+    public void setNomeHistorico(String nomeHistorico) {
+        this.nomeHistorico = nomeHistorico;
+    }
+
+    public HistoricoPais getHistoricoPais1() {
+        return historicoPais1;
+    }
+
+    public void setHistoricoPais1(HistoricoPais historicoPais1) {
+        this.historicoPais1 = historicoPais1;
+    }
+
+    public HistoricoPais getHistoricoPais2() {
+        return historicoPais2;
+    }
+
+    public void setHistoricoPais2(HistoricoPais historicoPais2) {
+        this.historicoPais2 = historicoPais2;
+    }
+
+    public String getDataHistorico() {
+        return dataHistorico;
+    }
+
+    public void setDataHistorico(String dataHistorico) {
+        this.dataHistorico = dataHistorico;
+    }
+
+    public String getDataIncial() {
+        return dataIncial;
+    }
+
+    public void setDataIncial(String dataIncial) {
+        this.dataIncial = dataIncial;
+    }
+
+    public String getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getConfirmadosDiferenca() {
+        return confirmadosDiferenca;
+    }
+
+    public void setConfirmadosDiferenca(long confirmadosDiferenca) {
+        this.confirmadosDiferenca = confirmadosDiferenca;
+    }
+
+    public long getMortesDiferenca() {
+        return mortesDiferenca;
+    }
+
+    public void setMortesDiferenca(long mortesDiferenca) {
+        this.mortesDiferenca = mortesDiferenca;
+    }
+
+    public long getRecuperadosDiferenca() {
+        return recuperadosDiferenca;
+    }
+
+    public void setRecuperadosDiferenca(long recuperadosDiferenca) {
+        this.recuperadosDiferenca = recuperadosDiferenca;
     }
 }
