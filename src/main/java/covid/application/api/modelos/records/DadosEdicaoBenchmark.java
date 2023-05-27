d36610471baeaaa9475c150ed8d79c6f2a05d305
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-26 23:33:39 -0300

    [IMPROVEMENT] Melhoria na forma de receber dados de benchmark para edição

--- src/main/java/covid/application/api/modelos/records/DadosEdicaoBenchmark.java
+++ src/main/java/covid/application/api/modelos/records/DadosEdicaoBenchmark.java
@@ -3,6 +3,8 @@ package covid.application.api.modelos.records;
 public record DadosEdicaoBenchmark(
         long id,
         String nomeBench,
+        String siglaPais1,
+        String siglaPais2,
         String dataInicial,
         String dataFinal
 ) {
