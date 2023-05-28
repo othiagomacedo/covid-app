3fe2e359fc3c3f389624ae966c245f615f7723a9
Chirana <96755607+othiagomacedo@users.noreply.github.com>
2023-05-28 18:37:05 -0300

    [IMPROVEMENT] V1 Tabelas Necessárias do Banco

--- src/main/resources/db.migration/V1__CRIAR_TABELAS_PRINCIPAIS.sql
+++ src/main/resources/db.migration/V1__CRIAR_TABELAS_PRINCIPAIS.sql
@@ -1,27 +1,32 @@
 CREATE TABLE historico_pais (
-        id SERIAL PRIMARY KEY,
-        data_inicial VARCHAR(40),
-        data_final VARCHAR(40),
-        confirmados BIGINT,
-        mortes BIGINT,
-        recuperados BIGINT,
-        ultimo_update VARCHAR(40),
-        percentual_fatalidade FLOAT,
-        pais_id BIGINT,
-        FOREIGN KEY (pais_id) REFERENCES pais (id)
+    id SERIAL PRIMARY KEY,
+    data_inicial VARCHAR(30),
+    data_final VARCHAR(30),
+    confirmados BIGINT,
+    mortes BIGINT,
+    recuperados BIGINT,
+    ultimo_update VARCHAR(30),
+    percentual_fatalidade DOUBLE PRECISION,
+    pais_id BIGINT REFERENCES pais(id)
 );
 
 CREATE TABLE pais (
-      id SERIAL PRIMARY KEY,
-      sigla VARCHAR(5) UNIQUE,
-      nome VARCHAR(50)
+    id SERIAL PRIMARY KEY,
+    sigla VARCHAR(5) UNIQUE,
+    nome VARCHAR(50)
 );
 
 CREATE TABLE historico_benchmark (
      id SERIAL PRIMARY KEY,
+     nome_historico VARCHAR(250) unique,
      historico_pais1_id BIGINT,
      historico_pais2_id BIGINT,
      data_historico VARCHAR(40),
+     data_inicial VARCHAR(40),
+     data_final VARCHAR(40),
+     confirmados_diferenca BIGINT,
+     mortes_diferenca BIGINT,
+     recuperados_diferenca BIGINT,
      FOREIGN KEY (historico_pais1_id) REFERENCES historico_pais (id),
      FOREIGN KEY (historico_pais2_id) REFERENCES historico_pais (id)
 );
